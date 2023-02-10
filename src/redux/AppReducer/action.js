be4b81845a3cfe24e36425d0483a810af784f62c
amitrajoria <amitrajoria.it@gmail.com>
2023-02-10 17:57:53 +0530

    Changed json-server from localhost to live url

--- src/redux/AppReducer/action.js
+++ src/redux/AppReducer/action.js
@@ -3,46 +3,46 @@ import * as actions from './actionTypes';
 
 const getTasks = () => dispatch => {
     dispatch({type : actions.TASKS_REQUEST});
-    return axios.get(`http://localhost:5000/tasks`)
+    return axios.get(`https://task-manager-api-qcfq.onrender.com/tasks`)
             .then((res) => dispatch({type: actions.TASKS_SUCCESS, payload: res.data}))
             .catch((err) => dispatch({type: actions.TASKS_FAILURE}))
 }
 
 const getTags = () => dispatch => {
-    return axios.get(`http://localhost:5000/tagList`)
+    return axios.get(`https://task-manager-api-qcfq.onrender.com/tagList`)
             .then((res) => dispatch({type: actions.TAGS_SUCCESS, payload: res.data}))
             .catch((err) => dispatch({type: actions.TAGS_FAILURE}))
 }
 
 const updateSubTaskStatus = (id, payload) => dispatch => {
-    return axios.patch(`http://localhost:5000/tasks/${id}`, payload)
+    return axios.patch(`https://task-manager-api-qcfq.onrender.com/tasks/${id}`, payload)
             .then((res) => dispatch(getTasks()))
             .catch((err) => dispatch({type: actions.TASKS_FAILURE}))
 }
 
 const updateTask = (id, payload) => (dispatch) => {
     dispatch({type : actions.UPDATE_TASK_REQUEST});
-    return axios.patch(`http://localhost:5000/tasks/${id}`, payload)
+    return axios.patch(`https://task-manager-api-qcfq.onrender.com/tasks/${id}`, payload)
             .then((res) => dispatch(getTasks()))
             .catch((err) => dispatch({type: actions.TASKS_FAILURE}))
 }
 
 const addTag = (payload) => (dispatch) => {
-    return axios.post(`http://localhost:5000/tagList`, payload)
+    return axios.post(`https://task-manager-api-qcfq.onrender.com/tagList`, payload)
             .then((res) => dispatch(getTags()))
             .catch((err) => dispatch({type: actions.TAGS_FAILURE}))
 }
 
 const createTask = (payload) => (dispatch) => {
     dispatch({type : actions.TASKS_REQUEST});
-    return axios.post(`http://localhost:5000/tasks`, payload)
+    return axios.post(`https://task-manager-api-qcfq.onrender.com/tasks`, payload)
             .then((res) => dispatch(getTasks()))
             .catch((err) => dispatch({type: actions.TASKS_FAILURE}))
 }
 
 const deleteTask = (id) => (dispatch) => {
     dispatch({type : actions.TASKS_REQUEST});
-    return axios.delete(`http://localhost:5000/tasks/${id}`)
+    return axios.delete(`https://task-manager-api-qcfq.onrender.com/tasks/${id}`)
             .then((res) => dispatch(getTasks()))
             .catch((err) => dispatch({type: actions.TASKS_FAILURE}))
 }
